0c18fdd48342167f4e35402da601b7319c4f6f03
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-08-25 22:50:52 -0300

    classe bootcamp

--- src/Bootcamp.java
+++ src/Bootcamp.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bootcamp {
+	
+	private Long id;
+	private String nome;
+	List<Devs> devs = new ArrayList<>();
+	
+	
 
 	public Bootcamp() {
 		// TODO Auto-generated constructor stub
