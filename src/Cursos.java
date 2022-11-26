99c5bdc950abb135e63d888edf8f295fc1493f6a
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-11-25 22:45:25 -0300

    Classe Cursos

--- src/Cursos.java
+++ src/Cursos.java
@@ -1,8 +1,73 @@
+import java.time.LocalDateTime;
 
 public class Cursos {
 
+	private Long id;
+	private String nomeCurso;
+	private String descricao;
+	private LocalDateTime tempo;
+	
+	
+	
+	public Cursos(String nomeCurso, String descricao, LocalDateTime tempo) {
+		
+		this.nomeCurso = nomeCurso;
+		this.descricao = descricao;
+		this.tempo = tempo;
+	}
+
+
+
 	public Cursos() {
 		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+
+
+
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+
+	public LocalDateTime getTempo() {
+		return tempo;
+	}
+
+
+
+	public void setTempo(LocalDateTime tempo) {
+		this.tempo = tempo;
 	}
 
 }
