2b4cdee4a10ef7b93a49b7c5c1d26238d210c04a
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-12-02 21:34:10 -0300

    classe mentoria

--- src/Mentorias.java
+++ src/Mentorias.java
@@ -1,8 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Mentorias {
-
-	public Mentorias() {
-		// TODO Auto-generated constructor stub
+	private Long id;
+	private String nomeMentoria;
+	public Mentorias(String nomeMentoria, Cursos cursos, List<Devs> devs) {
+		
+		this.nomeMentoria = nomeMentoria;
+		this.cursos = cursos;
+		this.devs = devs;
 	}
 
+
+	private Cursos cursos;
+	List<Devs> devs = new ArrayList<>();
+	
+	
+	public Mentorias() {
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNomeMentoria() {
+		return nomeMentoria;
+	}
+
+
+	public void setNomeMentoria(String nomeMentoria) {
+		this.nomeMentoria = nomeMentoria;
+	}
+
+
+	public Cursos getCursos() {
+		return cursos;
+	}
+
+
+	public void setCursos(Cursos cursos) {
+		this.cursos = cursos;
+	}
+
+
+	public List<Devs> getDevs() {
+		return devs;
+	}
+
+
+	public void setDevs(List<Devs> devs) {
+		this.devs = devs;
+	}
+	
 }
